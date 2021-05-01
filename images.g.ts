01ad92e41b75ff60bccd2e80de515c0206bb4076
Charlie <65603222+charlie-moomoo@users.noreply.github.com>
2021-05-01 22:14:30 +0800

    MakeCode 專案的初始檔案

---
+++ images.g.ts
@@ -0,0 +1,39 @@
+// 自動生成的代碼。請勿編輯。
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "Y[vi94Hs9Ybh-$4Ska2!":
+            case "Black":return img`
+..................
+..................
+.....fffffff......
+...fffffffffff....
+..fffffffffffff...
+..fffffffffffff...
+.fffffffffffffff..
+.fffffffffffffff..
+.fffffffffffffff..
+.fffffffffffffff..
+.fffffffffffffff..
+.fffffffffffffff..
+.fffffffffffffff..
+..fffffffffffff...
+..fffffffffffff...
+...fffffffffff....
+.....fffffff......
+..................
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// 自動生成的代碼。請勿編輯。
